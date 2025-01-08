f191d2a95e059f7bb4088965ebcb49039c218e9e
AlexCLeduc <AlexCLeduc@users.noreply.github.com>
2025-01-08 12:21:43 -0500

    fix logout and helpers warning (#10)

--- src/proj/views.py
+++ src/proj/views.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.views import LoginView as BaseLoginView
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import RedirectView
 
 from proj.middleware import AllowUnauthenticatedMixin
+
+
+class LogoutView(BaseLogoutView):
+    http_method_names = ["get", "post", "options"]
+
+    def get_success_url(self):
+        return reverse("login")
+
+    def get(self, *args, **kwargs):
+        return self.post(*args, **kwargs)
 
 
 def get_redirect_for_user(user):
